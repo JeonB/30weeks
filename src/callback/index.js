a2b00ba3d08aa63dd19b671780ac23dafcc90d2f
JeonB <newjeonb@gmail.com>
2025-10-15 16:44:17 +0900

    callback ex

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,19 @@ function hi(name) {
 }
 
 main(hi);
+
+function greetToUser(greet) {
+  const name = 'John';
+  greet(name);
+}
+
+function greetInKorean(name) {
+  console.log(`안녕하세요, ${name}!`);
+}
+
+function greetInEnglish(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greetToUser(greetInKorean);
+greetToUser(greetInEnglish);

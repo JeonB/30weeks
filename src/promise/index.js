22ad44c07b8b88b2e2ec4ceb872a46b83ad53810
JeonB <newjeonb@gmail.com>
2025-10-17 22:56:14 +0900

    promise

---
+++ src/promise/index.js
@@ -0,0 +1,23 @@
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () =>
+    callback(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+  document.head.append(script);
+}
+
+function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => resolve(script);
+    script.onerror = () =>
+      reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+    document.head.append(script);
+  });
+}

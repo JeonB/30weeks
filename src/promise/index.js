ee6163fdd8bec320eead6cca6c5110a4a20ec549
JeonB <newjeonb@gmail.com>
2025-10-18 23:04:30 +0900

    promise

--- src/promise/index.js
+++ src/promise/index.js
@@ -21,3 +21,11 @@ function loadScript(src) {
     document.head.append(script);
   });
 }
+
+let promise = new Promise(function (resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(console.log);

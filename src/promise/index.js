6cfaf20d5b282845ec1a2b52b8a622d3efb2a642
JeonB <newjeonb@gmail.com>
2025-10-21 10:45:53 +0900

    promise3

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,27 +1,3 @@
-function loadScript(src, callback) {
-  let script = document.createElement('script');
-  script.src = src;
-
-  script.onload = () => callback(null, script);
-  script.onerror = () =>
-    callback(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
-
-  document.head.append(script);
-}
-
-function loadScript(src) {
-  return new Promise(function (resolve, reject) {
-    let script = document.createElement('script');
-    script.src = src;
-
-    script.onload = () => resolve(script);
-    script.onerror = () =>
-      reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
-
-    document.head.append(script);
-  });
-}
-
 // executer 역할은 Promise 생성자 함수에 전달된 콜백 함수
 // 실행 결과에 따라 내부 프라퍼티인 state, result 값이 결정됨
 let promise = new Promise(function (resolve, reject) {
@@ -57,3 +33,72 @@ promise3
 promise2
   .finally(() => alert('프라미스가 준비되었습니다.'))
   .catch(err => alert(err)); // <-- .catch에서 에러 객체를 다룰 수 있음
+
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () =>
+    callback(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+  document.head.append(script);
+}
+
+function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => resolve(script);
+    script.onerror = () =>
+      reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+
+    document.head.append(script);
+  });
+}
+
+let promise4 = loadScript(
+  'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js'
+);
+
+promise4.then(
+  script => console.log(`${script.src}을 불러왔습니다!`),
+  error => console.log(`Error: ${error.message}`)
+);
+
+promise4.then(script => console.log('또다른 핸들러...'));
+
+showCircle(150, 150, 100).then(div => {
+  div.classList.add('message-ball');
+  div.append('Hello, world!');
+});
+
+function go() {
+  showCircle(150, 150, 100).then(div => {
+    div.classList.add('message-ball');
+    div.append('Hello, world!');
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  });
+}

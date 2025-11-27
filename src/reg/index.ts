231a14f98480486a66499bd3e8c30829fb6beb65
JeonB <newjeonb@gmail.com>
2025-11-27 18:15:40 +0900

    정규표현식 2

--- src/reg/index.ts
+++ src/reg/index.ts
@@ -8,3 +8,20 @@ function isNumberString(value: string) {
 // console.log(isNumberString(123));
 console.log(isNumberString('1242412412412423'));
 console.log(reg2.test('12222:'));
+
+let str = `1st place: Winnie
+2nd place: Piglet
+3rd place: Eeyore`;
+
+let reg3 = /^\d/gm;
+// let reg3 = /^\d/g;
+console.log(str.match(reg3));
+
+let str2 = `domino pizza 1
+burger king 2
+kfc 3`;
+let reg4 = /\d$/gm;
+console.log(str2.match(reg4));
+
+let reg5 = /\d\n/gm;
+console.log(str2.match(reg5));
